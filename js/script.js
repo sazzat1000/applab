96dcb34cf81f87ecc21cc3752e79d7793f53ecc9
sazzat1000 <sazzat1000@gmail.com>
2020-09-07 19:46:21 +0600

    Done

--- js/script.js
+++ js/script.js
@@ -9,5 +9,38 @@ $(document).ready(function () {
     nextArrow: '<i class="fas fa-long-arrow-alt-right arrow arrow-right"></i>',
   });
   new WOW().init();
+
+
+   // Sticky Nav
+   var menu_top_offset = $("header").offset().top;
+
+   $(window).on("scroll", function () {
+     var scrollSize = $(window).scrollTop();
+ 
+     if (scrollSize > menu_top_offset) {
+       $("header").addClass("sticky-header");
+       $("body").css("margin-top", "71px");
+     } else {
+       $("header").removeClass("sticky-header");
+       $("body").css("margin-top", "0");
+     }
+   });
+
+  // Smooth Scroll
+  $("a").on("click", function (event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
   
 });
